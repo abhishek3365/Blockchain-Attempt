734c5340e479280ed17c6868d03a7779936e29c0
Abhishek Sharma <dhanvantriabhi@gmail.com>
2018-04-15 15:57:19 +0530

    blockchain class added

---
+++ blockchain.js
@@ -0,0 +1,45 @@
+const Block = require('./block');
+
+class BlockChain {
+    constructor () {
+        this.chain = [Block.genesis()];
+    }
+
+    addBlock ( data ) {
+        const block = Block.mineBlock( this.chain[this.chain.length-1] , data );
+        this.chain.push( block );
+        return block;
+    }
+
+    isValidChain ( chain ) {
+        if( JSON.stringify( chain[0] ) !== JSON.stringify( Block.genesis() ) )
+            return false;
+        
+        for (let index = 1; index < chain.length; index++) {
+            const block = chain[index];
+            const lastBlock = chain[index-1];
+
+            if( block.lastHash !== lastBlock.hash || block.hash !== Block.blockHash(block) )
+                return false;
+        }
+
+        return true;
+    }
+
+    replaceChain ( newChain ){
+        if( newChain.length <= this.chain.length ){
+            console.log('Recieved chain is not longer than the current chain');
+            return;
+        }
+        else if( !this.isValidChain(newChain) ) {
+            console.log('Recieved chain is not valid');
+            return;
+        }
+
+        console.log('Replacing blockchain with new chain');
+        this.chain = newChain;
+
+    } 
+}
+
+module.exports = BlockChain;
