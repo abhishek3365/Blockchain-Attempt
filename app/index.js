cce454ac6d3dfa7a0d528482a90d42d24022de01
EMEAAD\abhissharma <abhishek.sharma3@soprasteria.com>
2018-04-23 09:49:02 +0530

    mine api created

--- app/index.js
+++ app/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyparser = require('body-parser')
 const Blockchain = require('../blockchain');
 
 const PORT = process.env.HTTP_PORT || 3000;
@@ -6,9 +7,17 @@ const PORT = process.env.HTTP_PORT || 3000;
 const app = express();
 const bc = new Blockchain();
 
+app.use( bodyparser.json() );
+
 app.get('/blocks' , (req,res) => {
     res.json(bc.chain);
 });
+
+app.post( '/mine' , (req,res) => {
+    const block =  bc.addBlock(req.body.data);
+    console.log(`New block added : ${block.toString()}`);
+    res.redirect('/blocks');
+} )
 
 app.listen( PORT , () =>{
     console.log(`Server running at port ${PORT}`);
