7f4f425e402fb8e6b7dbf80a7744272fbdf07d0f
EMEAAD\abhissharma <abhishek.sharma3@soprasteria.com>
2018-04-23 12:09:08 +0530

    creating network

--- app/index.js
+++ app/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyparser = require('body-parser')
 const Blockchain = require('../blockchain');
+const P2PServer = require('./p2p-server');
 
 const PORT = process.env.HTTP_PORT || 3000;
 
 const app = express();
 const bc = new Blockchain();
+const p2pServer = new P2PServer(bc);
 
 app.use( bodyparser.json() );
 
@@ -16,9 +18,9 @@ app.get('/blocks' , (req,res) => {
 app.post( '/mine' , (req,res) => {
     const block =  bc.addBlock(req.body.data);
     console.log(`New block added : ${block.toString()}`);
+    p2pServer.syncChange();
     res.redirect('/blocks');
 } )
 
-app.listen( PORT , () =>{
-    console.log(`Server running at port ${PORT}`);
-} );
+app.listen( PORT , () => console.log(`Server running at port ${PORT}`) );
+p2pServer.listen();
